fb8b03c30038d84b40b80db846a084d98387f1dc
pereira-dc <davicavalcantework@gmail.com>
2026-03-17 07:58:17 -0300

    att main.cpp

--- main.cpp
+++ main.cpp
@@ -2,59 +2,66 @@
 #include <string>
 #include <vector>
 #include <map>
-#include "lista_compras.h"
+#include "dados_vendas.hpp"
+#include "similaridade.hpp"
 
 using namespace std;
 
-int main(){
+int main() {
+    Dados bd = ListaCompras("dados_venda_cluster_0.csv");
+    if (bd.codClientes.empty()) {
+        printf("Erro: nenhum cliente carregado.\n");
+        return 1;
+    }
 
-    string caminho = "coloque aqui o caminho do arquivo no seu computador";
+    vector<vector<int>> A = criarMatrizDensa(bd);
+    vector<vector<int>> I = multiplicarMatrizes(A);
+    vector<vector<double>> S = calcularSimilaridade(bd, I);
 
-    Dados dados = ListaCompras(caminho);
-
-    int opcao = 1;
-
-    do{
-        printf("\nDigite 1 se você quer pesquisar um cliente ou digite 0 para encerrar o programa: ");
+    int opcao;
+    do {
+        printf("\n--- MENU ---\n");
+        printf("1. Historico de compras (Atividade 1)\n");
+        printf("2. Similaridade entre dois clientes (Atividade 2)\n");
+        printf("0. Sair\n");
+        printf("Escolha: ");
         scanf("%d", &opcao);
 
-        if (opcao == 1){
-
-            char cd[9];
-            printf("\nDigite o código do cliente: ");
-            scanf("%8s", cd);
-
-            string codigo = cd; //passa para string do C++
-
-            if (verificador(dados.codClientes, codigo) == true){
-
-                int indice_cliente = dados.mapClientes[codigo]; 
-
-                printf("Produto(s) comprado(s) pelo cliente: \n\n");
-
-                for (size_t i = 0; i < dados.comprasPorCliente[indice_cliente].size(); i++){ //a função .size() retorna um valor de tipo size_t, então o i tem que ser declarado como size_t tbm. Ou senão pode colocar um (int) antes do dados.comprasPorCliente
-                //o for tem o tamanho do número de produtos comprado pelo cliente
-
-                    int indice_produto = dados.comprasPorCliente[indice_cliente][i]; //recebe o indice de um produto por vez 
-
-                    printf("%s\n", dados.nomeProdutos[indice_produto].c_str()); //printa o nome do produto acessado
-                    
-                }
-                
+        if (opcao == 1) {
+            char cod[9];
+            printf("Codigo do cliente: ");
+            scanf("%8s", cod);
+            if (bd.mapClientes.count(cod) == 0) {
+                printf("Cliente nao encontrado.\n");
+                continue;
             }
-
-            else{
-                printf("Esse código não está listado\n");
+            int id = bd.mapClientes[cod];
+            printf("Produtos comprados:\n");
+            const vector<int>& compras = bd.comprasPorCliente[id];
+            for (size_t i = 0; i < compras.size(); i++) {
+                int p = compras[i];
+                printf("- %s\n", bd.nomeProdutos[p].c_str());
             }
-
-
         }
+        else if (opcao == 2) {
+            char cod1[9], cod2[9];
+            printf("Digite o codigo do primeiro cliente: ");
+            scanf("%8s", cod1);
+            printf("Digite o codigo do segundo cliente: ");
+            scanf("%8s", cod2);
 
-        
+            if (bd.mapClientes.count(cod1) == 0 || bd.mapClientes.count(cod2) == 0) {
+                printf("Um ou ambos os clientes nao existem.\n");
+                continue;
+            }
 
+            int id1 = bd.mapClientes[cod1];
+            int id2 = bd.mapClientes[cod2];
+
+            double sim = obterSimilaridade(bd, S, id1, id2);
+            printf("Similaridade (Jaccard) do cliente %s em relacao a %s: %.4f\n", cod1, cod2, sim);
+        }
     } while (opcao != 0);
-
-    printf("Programa encerrado!!\n");
 
     return 0;
 }
