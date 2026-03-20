27826ab4b24f51daf9c92ee944f6f4589c18c4a1
pereira-dc <davicavalcantework@gmail.com>
2026-03-20 15:19:49 -0300

    Refactor ListaCompras function for clarity

--- dados_vendas.cpp
+++ dados_vendas.cpp
@@ -1,7 +1,7 @@
 #include "dados_vendas.hpp"
 #include <stdio.h>
 
-Dados ListaCompras(string pathArquivo) {
+Dados ListaCompras(string pathArquivo) { //usar char e passar por ref
     Dados dados;
     FILE *arquivo = fopen(pathArquivo.c_str(), "r");
     if (arquivo == NULL) {
@@ -14,18 +14,21 @@ Dados ListaCompras(string pathArquivo) {
 
     char cliente[9], produto[50];
     while (fscanf(arquivo, "%*d,%8[^,],%*d,%49[^\n]\n", cliente, produto) == 2) {
-        if (dados.mapClientes.count(cliente) == 0) {
+        if (dados.mapClientes.count(cliente) == 0) { //usar .find()
             dados.mapClientes[cliente] = dados.codClientes.size();
             dados.codClientes.push_back(cliente);
         }
         if (dados.mapProdutos.count(produto) == 0) {
             dados.mapProdutos[produto] = dados.nomeProdutos.size();
             dados.nomeProdutos.push_back(produto);
+            // dados.comprasPorCliente.push_back(vector<int>());
         }
+
+        
     }
 
-    rewind(arquivo);
-    fgets(cabecalho, sizeof(cabecalho), arquivo);
+    rewind(arquivo);//
+    fgets(cabecalho, sizeof(cabecalho), arquivo); 
 
     dados.comprasPorCliente.resize(dados.codClientes.size());
 
