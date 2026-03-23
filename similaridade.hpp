90092e16f249434bdf09844008b71c5fcc11736b
pereira-dc <davicavalcantework@gmail.com>
2026-03-23 11:38:28 -0300

    Add efficient matrix multiplication and similarity functions

--- similaridade.hpp
+++ similaridade.hpp
@@ -7,7 +7,10 @@ using namespace std;
 
 vector<vector<int>> criarMatrizDensa(const Dados& dados);
 vector<vector<int>> multiplicarMatrizes(const vector<vector<int>>& A);
+vector<vector<int>> multiplicarEficiente(const vector<vector<int>>& A);
 vector<vector<double>> calcularSimilaridade(const Dados& dados, const vector<vector<int>>& I);
+vector<vector<double>> calcularSimEfi(const Dados& dados, const vector<vector<int>>& I); 
 double obterSimilaridade(const Dados& dados, const vector<vector<double>>& S, int i, int j);
+double obterSimEfi(const Dados& dados, const vector<vector<double>>& SE, int i, int j);
 
 #endif
