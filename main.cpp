483393cd253c78888dfd0d75eed26109f19c7a74
pereira-dc <davicavalcantework@gmail.com>
2026-03-23 11:36:43 -0300

    Refactor similarity calculation to use efficient algorithms

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <vector>
 #include <map>
+#include <ctime> 
 #include "dados_vendas.hpp"
 #include "similaridade.hpp"
 #include "recomendacao.hpp"
@@ -16,8 +17,12 @@ int main() {
     }
 
     vector<vector<int>> A = criarMatrizDensa(bd);
-    vector<vector<int>> I = multiplicarMatrizes(A);
-    vector<vector<double>> S = calcularSimilaridade(bd, I);
+    
+    vector<vector<int>> I_padrao = multiplicarMatrizes(A);
+    vector<vector<double>> S_normal = calcularSimilaridade(bd, I_padrao);
+    
+    vector<vector<int>> I_eficiente = multiplicarEficiente(A);
+    vector<vector<double>> S_eficiente = calcularSimEfi(bd, I_eficiente);
 
     int opcao;
     do {
@@ -47,6 +52,14 @@ int main() {
         }
         else if (opcao == 2) {
             char cod1[9], cod2[9];
+            int algoritmo;
+            
+            printf("Escolha o tipo da multiplicacao de similaridade:\n");
+            printf("1 - Multiplicacao normal\n");
+            printf("2 - Multiplicacao eficiente\n");
+            printf("Opcao: ");
+            scanf("%d", &algoritmo);
+            
             printf("Digite o codigo do primeiro cliente: ");
             scanf("%8s", cod1);
             printf("Digite o codigo do segundo cliente: ");
@@ -59,13 +72,29 @@ int main() {
 
             int id1 = bd.mapClientes[cod1];
             int id2 = bd.mapClientes[cod2];
-
-            double sim = obterSimilaridade(bd, S, id1, id2);
+            double sim;
+            
+            clock_t inicio = clock();
+            
+            if (algoritmo == 1) {
+                printf("\nUsando algoritmo normal...\n");
+                sim = obterSimilaridade(bd, S_normal, id1, id2);
+            } else {
+                printf("\nUsando algoritmo eficiente...\n");
+                sim = obterSimEfi(bd, S_eficiente, id1, id2);
+            }
+            
+            clock_t fim = clock();
+            double tempo = double(fim - inicio) / CLOCKS_PER_SEC;
+            
             printf("Similaridade (Jaccard) do cliente %s em relacao a %s: %.4f\n", cod1, cod2, sim);
+            printf("Tempo de execucao: %.6f segundos\n", tempo);
         }
         else if (opcao == 3){
             char cod1[9], cod2[9], cod3[9];
             int k;
+            
+            printf("Usando algoritmo eficiente para recomendacoes...\n");
             printf("Digite o codigo do primeiro cliente: ");
             scanf("%8s", cod1);
             printf("Digite o codigo do segundo cliente: ");
@@ -74,7 +103,6 @@ int main() {
             scanf("%8s", cod3);
             printf("Digite o número de produtos recomendados que deseja ver: ");
             scanf("%d", &k);
-
 
             if (bd.mapClientes.count(cod1) == 0 || bd.mapClientes.count(cod2) == 0 || bd.mapClientes.count(cod3) == 0) {
                 printf("Um ou mais clientes nao existem.\n");
@@ -85,9 +113,9 @@ int main() {
             int id2 = bd.mapClientes[cod2];
             int id3 = bd.mapClientes[cod3];
 
-            vector<RankProduto> rankcl1 = recomendacao(id1, bd, S, k);
-            vector<RankProduto> rankcl2 = recomendacao(id2, bd, S, k);
-            vector<RankProduto> rankcl3 = recomendacao(id3, bd, S, k);
+            vector<RankProduto> rankcl1 = recomendacao(id1, bd, S_eficiente, k);
+            vector<RankProduto> rankcl2 = recomendacao(id2, bd, S_eficiente, k);
+            vector<RankProduto> rankcl3 = recomendacao(id3, bd, S_eficiente, k);
             
             printf("\nPrimeiro cliente: \n");
             for (int i = 0; i < k; i++){
